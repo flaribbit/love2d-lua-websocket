ff4425acec72bfc7433c03e5b7bde4a820e42cef
flaribbit <1149761294@qq.com>
2021-02-13 22:10:35 +0800

    update

--- websocket.lua
+++ websocket.lua
@@ -1,21 +1,31 @@
+--[[
+    websocket client pure lua implement for love2d
+    by flaribbit
+
+    usage:
+        local client = require("websocket").new()
+        client:settimeout(1)
+        client:connect("127.0.0.1", 5000)
+        client:settimeout(0)
+        client:send("hello from love2d")
+        res, opcode = client:read()
+        print(res)
+        client:close()
+]]
+
+-- local debug_print=print
 local socket = require"socket"
 local bit = require"bit"
-local band = bit.band
-local bor = bit.bor
-local bxor = bit.bxor
-local shl = bit.lshift
-local shr = bit.rshift
-
-local log_debug = print
-local b2s = function(b)return b and"true"or"false"end
+local band, bor, bxor = bit.band, bit.bor, bit.bxor
+local shl, shr = bit.lshift, bit.rshift
 
 local OPCODES = {
-    ["CONTINUE"]=0,
-    ["TEXT"]    =1,
-    ["BINARY"]  =2,
-    ["CLOSE"]   =8,
-    ["PING"]    =9,
-    ["PONG"]    =10,
+    CONTINUE=0,
+    TEXT    =1,
+    BINARY  =2,
+    CLOSE   =8,
+    PING    =9,
+    PONG    =10,
 }
 
 local _M = {
@@ -23,98 +33,112 @@ local _M = {
 }
 _M.__index = _M
 
-_M.new = function()
-    local m = {}
+function _M.new()
+    local m = {socket = socket.tcp()}
     setmetatable(m, _M)
     return m
 end
 
-_M.connect = function(self, host, port)
-    local socket = socket.tcp()
-    self.socket = socket
-    local res, err = socket:connect(host, port)
-    log_debug("[handshake] connected")
-    if res~=1 then
-        return res, err
-    end
-    -- WebSocket握手
-    res, err = socket:send("GET / HTTP/1.1\r\nHost: "..host..":"..port.."\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: osT3F7mvlojIvf3/8uIsJQ==\r\n\r\n")
-    repeat
-        res = socket:receive("*l")
-    until res==""
-    log_debug("[handshake] succeed")
+local seckey = "osT3F7mvlojIvf3/8uIsJQ=="
+function _M:connect(host, port, path)
+    local SOCK = self.socket
+    local res, err = SOCK:connect(host, port)
+    if res~=1 then return res, err end
+    -- debug_print("[handshake] connected")
+
+    -- WebSocket handshake
+    res, err = SOCK:send("GET "..(path or"/").." HTTP/1.1\r\nHost: "..host..":"..port.."\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: "..seckey.."\r\n\r\n")
+    repeat res = SOCK:receive("*l") until res==""
+    -- debug_print("[handshake] succeed")
 end
 
-_M.send = function(self, message)
-    local socket = self.socket
-    local mask_key = {1, 14, 5, 14}
-    -- 消息类型
-    socket:send(string.char(bor(0x80, OPCODES.BINARY)))
-    -- 编码长度
+local mask_key = {1, 14, 5, 14}
+local function send(SOCK, opcode, message)
+    -- message type
+    SOCK:send(string.char(bor(0x80, opcode)))
+
+    if not message then
+        SOCK:send(string.char(0x80, unpack(mask_key)))
+        return 0
+    end
+
+    -- length
     local length = #message
-    log_debug("[encode] message length: "..length)
+    -- debug_print("[encode] message length: "..length)
     if length>65535 then
-        socket:send(string.char(bor(127, 0x80), 0, 0, 0, 0,
+        SOCK:send(string.char(bor(127, 0x80), 0, 0, 0, 0,
             band(shr(length, 24), 0xff),
             band(shr(length, 16), 0xff),
             band(shr(length, 8), 0xff),
             band(length, 0xff)))
     elseif length>125 then
-        socket:send(string.char(bor(126, 0x80),
+        SOCK:send(string.char(bor(126, 0x80),
             band(shr(length, 8), 0xff),
             band(length, 0xff)))
     else
-        socket:send(string.char(bor(length, 0x80)))
+        SOCK:send(string.char(bor(length, 0x80)))
     end
-    log_debug("[encode] masking")
-    socket:send(string.char(unpack(mask_key)))
+    -- debug_print("[encode] masking")
+    SOCK:send(string.char(unpack(mask_key)))
     local msgbyte = {message:byte(1, length)}
     for i = 1, length do
         msgbyte[i] = bxor(msgbyte[i], mask_key[(i-1)%4+1])
     end
-    socket:send(string.char(unpack(msgbyte)))
-    log_debug("[encode] end")
+    return SOCK:send(string.char(unpack(msgbyte)))
+    -- debug_print("[encode] end")
 end
 
-_M.read = function(self)
-    -- 解码数据
-    -- 0-1字节
-    local socket = self.socket
-    local res, err = socket:receive(2)
-    if res==nil then
-        return res, err
-    end
-    local byte = res:byte()
-    local flag_FIN = byte>=0x80
-    local OPCODE = band(byte, 0x0f)
-    byte = res:byte(2)
-    local flag_MASK = byte>=0x80
-    log_debug("[decode] FIN="..b2s(flag_FIN)..", OPCODE="..OPCODE..", MASK="..b2s(flag_MASK))
-    -- 解码长度
+function _M:send(message)
+    send(self.socket, OPCODES.BINARY, message)
+end
+
+function _M:ping(message)
+    send(self.socket, OPCODES.PING, message)
+end
+
+function _M:pong(message)
+    send(self.socket, OPCODES.PONG, message)
+end
+
+function _M:read()
+    -- byte 0-1
+    local SOCK = self.socket
+    local res, err = SOCK:receive(2)
+    if not res then return res, err end
+
+    local OPCODE = band(res:byte(), 0x0f)
+    -- local flag_FIN = res:byte()>=0x80
+    -- local flag_MASK = res:byte(2)>=0x80
+    -- debug_print("[decode] FIN="..tostring(flag_FIN)..", OPCODE="..OPCODE..", MASK="..tostring(flag_MASK))
+
+    -- length
+    local byte = res:byte(2)
     local length = band(byte, 0x7f)
     if length==126 then
-        res = socket:receive(2)
+        res = SOCK:receive(2)
         local b1, b2 = res:byte(1, 2)
         length = shl(b1, 8) + b2
     elseif length==127 then
-        res = socket:receive(8)
+        res = SOCK:receive(8)
         local b = {res:byte(1, 8)}
         length = shl(b[5], 32) + shl(b[6], 24) + shl(b[7], 8) + b[8]
     end
-    log_debug("[decode] message length: "..length)
-    -- 接收数据
-    res = socket:receive(length)
-    log_debug("[decode] string length: "..#res)
-    log_debug("[decode] end")
-    return res
+    -- debug_print("[decode] message length: "..length)
+
+    -- data
+    res = SOCK:receive(length)
+    if OPCODE==OPCODES.PING then
+        self:pong(res)
+    elseif OPCODE==OPCODES.CLOSE then
+        self:close()
+    end
+    -- debug_print("[decode] string length: "..#res)
+    -- debug_print("[decode] end")
+    return res, OPCODE
 end
 
-_M.close = function(self)
-    self.socket:close()
-end
+function _M:close() self.socket:close() end
 
-_M.settimeout = function(self, t)
-    self.socket:settimeout(t)
-end
+function _M:settimeout(t) self.socket:settimeout(t) end
 
 return _M
